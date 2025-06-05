1b4d4fe4f7079df4e9c1200cd8d0c04c02219bd8
Alessio <pernici.alessio12@gmail.com>
2025-06-05 22:21:59 +0200

    add torcs and src

---
+++ torcs/src/torcs/blue/BlueDriver.java
@@ -0,0 +1,12 @@
+package torcs.blue;
+
+import torcs.color.ColorDriver_;
+
+public class BlueDriver extends ColorDriver_ {
+
+	@Override
+	public String getParametersPath() {
+		return "./src/torcs/blue/parameters.properties";
+	}
+
+}
---
+++ torcs/src/torcs/red/RedDriver.java
@@ -0,0 +1,12 @@
+package torcs.red;
+
+import torcs.color.ColorDriver_;
+
+public class RedDriver extends ColorDriver_ {
+
+	@Override
+	public String getParametersPath() {
+		return "./src/torcs/red/parameters.properties";
+	}
+
+}
